99b31cf81ed1311e4ad2faac8608ee08d588926c
MNJLou <jeromelou502@gmail.com>
2026-03-23 16:46:49 +0200

    Fixed Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
+  plugins: [
+    cloudflare()
+  ],
   server: {
     port: 5173,
     open: true
